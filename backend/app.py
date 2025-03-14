5c10df4a56b21b821d23d9c6e290659f10d12301
RakshithJoseph555 <rakshithmanaloor@gmail.com>
2025-03-14 11:49:59 +0530

    second

--- backend/app.py
+++ backend/app.py
@@ -1,112 +1,4 @@
-# from flask import Flask, request, jsonify
-# from pymongo import MongoClient
-# from azure.storage.blob import BlobServiceClient
-# from werkzeug.utils import secure_filename
-# from flask_cors import CORS
-# from bson import ObjectId
 
-# app = Flask(__name__)
-# CORS(app)
-
-# AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=listimgstore;AccountKey=OMi71/1lGbb1uidvn/fxwTUGyCnWBZa2cx50uHSkuyMq5v81vLYFTW4212t66ORMO0X/j21xe9hv+ASt9Uvq9Q==;EndpointSuffix=core.windows.net"
-# CONTAINER_NAME = "product-images"
-# blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
-
-# # MongoDB Connection (Replace with your actual connection string)
-# MONGO_URI = "mongodb://products-server:cyngGEiT2N0mbVdoz5CqVr2jCxpj3p5oaVdNBRWACtQErdfkjR9h7eAJ5628noQDk2Gdkh4w7RoKACDbMQkqaQ==@products-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@products-server@"
-# client = MongoClient(MONGO_URI)
-# db = client["product_db"]
-# collection = db["products"]
-
-# # # Add a Product
-# # @app.route('/add_product', methods=['POST'])
-# # def add_product():
-# #     data = request.json
-# #     product_id = collection.insert_one(data).inserted_id
-# #     return jsonify({"message": "Product added", "product_id": str(product_id)}), 201
-
-# @app.route("/add_product", methods=["POST"])
-# def add_product():
-#     name = request.form.get("name")
-#     price = float(request.form.get("price", 0))
-#     category = request.form.get("category")
-    
-#     image_url = None
-#     if "image" in request.files:
-#         image = request.files["image"]
-#         filename = secure_filename(image.filename)
-#         image_url = upload_image(image)
-
-#     product = {
-#         "name": name,
-#         "price": price,
-#         "category": category,
-#         "imageUrl": image_url
-#     }
-
-#     product_id = collection.insert_one(product).inserted_id
-#     return jsonify({"message": "Product added", "product_id": str(product_id), "imageUrl": image_url}), 201
-
-# # List All Products
-# @app.route('/list_products', methods=['GET'])
-# def list_products():
-#     products = list(collection.find({}))
-#     for product in products:
-#         product["_id"] = str(product["_id"])  # Convert ObjectId to string
-#     return jsonify(products), 200
-
-# # Update a Product
-# @app.route('/update_product/<product_id>', methods=['PUT'])
-# def update_product(product_id):
-#     data = request.json
-#     if "_id" in data:
-#         data.pop("_id")
-#     result = collection.update_one({"_id": ObjectId(product_id)}, {"$set": data})
-    
-#     if result.matched_count == 0:
-#         return jsonify({"error": "Product not found"}), 404
-#     return jsonify({"message": "Product updated"}), 200
-
-# # Delete a Product
-# @app.route('/delete_product/<product_id>', methods=['DELETE'])
-# def delete_product(product_id):
-#     result = collection.delete_one({"_id": ObjectId(product_id)})
-    
-#     if result.deleted_count == 0:
-#         return jsonify({"error": "Product not found"}), 404
-#     return jsonify({"message": "Product deleted"}), 200
-
-# # Clear All Products
-# @app.route('/clear_products', methods=['DELETE'])
-# def clear_products():
-#     collection.delete_many({})
-#     return jsonify({"message": "All products deleted"}), 200
-
-
-# @app.route('/upload_image', methods=['POST'])
-# def upload_image():
-#     if 'image' not in request.files:
-#         return jsonify({"error": "No image uploaded"}), 400
-
-#     image = request.files['image']
-#     filename = secure_filename(image.filename)
-
-#     try:
-#         # Upload to Azure Blob Storage
-#         blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=filename)
-#         blob_client.upload_blob(image, overwrite=True)
-
-#         # Get Image URL
-#         image_url = f"https://{blob_service_client.account_name}.blob.core.windows.net/{CONTAINER_NAME}/{filename}"
-
-#         return jsonify({"image_url": image_url}), 200
-
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
@@ -114,19 +6,21 @@ from azure.storage.blob import BlobServiceClient,generate_blob_sas, BlobSasPermi
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
 from bson import ObjectId
+from dotenv import load_dotenv
+import os
 from datetime import datetime, timedelta
-
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=listimgstore;AccountKey=OMi71/1lGbb1uidvn/fxwTUGyCnWBZa2cx50uHSkuyMq5v81vLYFTW4212t66ORMO0X/j21xe9hv+ASt9Uvq9Q==;EndpointSuffix=core.windows.net"
-CONTAINER_NAME = "product-images"
-ACCOUNT_NAME = "listimgstore"
-ACCOUNT_KEY="OMi71/1lGbb1uidvn/fxwTUGyCnWBZa2cx50uHSkuyMq5v81vLYFTW4212t66ORMO0X/j21xe9hv+ASt9Uvq9Q=="
+AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+CONTAINER_NAME = os.getenv("CONTAINER_NAME")
+ACCOUNT_NAME = os.getenv("ACCOUNT_NAME")
+ACCOUNT_KEY=os.getenv("ACCOUNT_KEY")
 blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
 
 # MongoDB Connection
-MONGO_URI = "mongodb://products-server:cyngGEiT2N0mbVdoz5CqVr2jCxpj3p5oaVdNBRWACtQErdfkjR9h7eAJ5628noQDk2Gdkh4w7RoKACDbMQkqaQ==@products-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@products-server@"
+MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["product_db"]
 collection = db["products"]
